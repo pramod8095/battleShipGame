943fc279db809776dd08fe71cb7ebd5a17c82df6
pramod <kpramod8095@allstate.com>
2022-06-18 14:29:05 +0530

    new

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function abc() {
     guesses +
     " guesses to sink the battleship," +
     " which means your shooting accuracy was " +
-    (3 / guesses) * 100 +
+    Math.floor(3 / guesses) * 100 +
     "%";
 
   alert(stats);
